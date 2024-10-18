e16fafd63af8a10b51f9e46509ecbcece74566d7
Ali Imron <65119796+amrron@users.noreply.github.com>
2024-10-18 15:48:52 +0000

    fix: bmi chart

--- app/Http/Controllers/BmiController.php
+++ app/Http/Controllers/BmiController.php
@@ -60,7 +60,7 @@ class BmiController extends Controller
         return response()->json([
             'success' => true,
             'status' => 'success',
-            'message' => $this->apichart(),
+            'message' => 'Berhasil mendapatkan data BMI terbaru',
             'data' => BmiResourece::collection($bmi)
         ], 200);
     }
@@ -81,7 +81,7 @@ class BmiController extends Controller
         return response()->json([
             'success' => true,
             'status' => 'success',
-            'message' => 'Data bmi terbaru',
+            'message' => $this->apichart(),
             'data' => $data
         ], 200);
     }
