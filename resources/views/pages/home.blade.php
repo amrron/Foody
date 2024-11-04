6b00646560ec82a38a22bdc68ef2ffc9063740d2
amrron <imrona463@gmail.com>
2024-11-04 14:42:15 +0700

    feat: download apk

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -617,7 +617,7 @@
                                 <path fill-rule="evenodd" d="M9.7 15.9 7.4 13H5a2 2 0 0 0-2 2v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2h-2.4l-2.3 2.9a3 3 0 0 1-4.6 0Zm7.3.1a1 1 0 1 0 0 2 1 1 0 1 0 0-2Z" clip-rule="evenodd"/>
                             </svg>                            
                         </div>
-                        <a href="/" class="flex items-center text-biru text-2xl md:text-5xl font-bold">
+                        <a href="/download" class="flex items-center text-biru text-2xl md:text-5xl font-bold">
                             Foody.apk
                         </a>
                     </div>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.home');
 });
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', function () {
+        return view('pages.admin.dashboard');
+    });
+});
+
+Route::get('/download', function(){
+    return response()->download(public_path("/app/Foody.apk"));
+});
